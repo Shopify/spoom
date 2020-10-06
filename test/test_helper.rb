dceacd6dac3a81d8c54cd30d978f3ff8fe405251
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2020-10-06 13:19:29 -0400

    Introduce TestProject to create projects on the fly when testing
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,37 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
 require "spoom"
 
+require "test_project"
+
 module Spoom
   module TestHelper
-    # Run an action before all tests only once
-    def before_all
-      unless @before_all
-        yield
-        @before_all = true
-      end
+    extend T::Sig
+
+    TEST_PROJECTS_PATH = "/tmp/spoom/tests"
+
+    sig { params(name: String).returns(TestProject) }
+    def spoom_project(name)
+      project = TestProject.new("#{TEST_PROJECTS_PATH}/#{name}")
+      project.gemfile(spoom_gemfile)
+      project
+    end
+
+    sig { returns(String) }
+    def spoom_gemfile
+      <<~GEM
+        gem("spoom", path: "#{spoom_path}")
+      GEM
+    end
+
+    sig { returns(String) }
+    def spoom_path
+      path = File.dirname(__FILE__)   # spoom/test/spoom/
+      path = File.dirname(path)       # spoom/test/
+      path = File.dirname(path)       # spoom/
+      File.expand_path(path)
     end
   end
 end
