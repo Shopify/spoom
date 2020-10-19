08aaebefe44884054376c2f0e8836d0a162bd4dc
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2020-10-19 12:21:40 -0400

    Extract Erb templates from Coverage::Report
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/spoom/report.rb
+++ lib/spoom/report.rb
@@ -3,10 +3,9 @@
 
 require_relative "file_tree"
 require_relative "snapshot"
-require_relative "coverage/d3"
+require_relative "coverage/report"
 
 require "date"
-require "erb"
 
 module Spoom
   module Coverage
@@ -14,13 +13,15 @@ module Spoom
 
     sig { params(snapshots: T::Array[Snapshot], path: String).returns(Report) }
     def self.report(snapshots, path: ".")
-      intro = T.must(Git.sorbet_intro_commit(path: path))
+      intro_commit = Git.sorbet_intro_commit(path: path)
+      intro_date = intro_commit ? Git.commit_time(intro_commit, path: path) : nil
+
       Report.new(
-        title: File.basename(File.expand_path(path)),
-        sorbet_intro_commit: intro,
-        sorbet_intro_date: T.must(Git.commit_time(intro, path: path)),
+        project_name: File.basename(File.expand_path(path)),
+        snapshots: snapshots,
         sigils_tree: sigils_tree(path: path),
-        snapshots: snapshots
+        sorbet_intro_commit: intro_commit,
+        sorbet_intro_date: intro_date,
       )
     end
 
@@ -35,123 +36,66 @@ module Spoom
       FileTree.new(files)
     end
 
-    class Report < T::Struct
+    class Report < Page
       extend T::Sig
 
-      prop :title, String, default: "Typing Coverage"
-      prop :sorbet_intro_commit, String
-      prop :sorbet_intro_date, Time
-      prop :sigils_tree, FileTree
-      prop :snapshots, T::Array[Snapshot]
-
-      sig { returns(Snapshot) }
-      def last
-        T.must(snapshots.last)
-      end
-
       sig { returns(String) }
-      def html
-        ERB.new(erb).result(get_binding)
+      attr_reader :project_name
+
+      sig { returns(T.nilable(String)) }
+      attr_reader :sorbet_intro_commit
+
+      sig { returns(T.nilable(Time)) }
+      attr_reader :sorbet_intro_date
+
+      sig { returns(T::Array[Snapshot]) }
+      attr_reader :snapshots
+
+      sig { returns(FileTree) }
+      attr_reader :sigils_tree
+
+      sig do
+        params(
+          project_name: String,
+          snapshots: T::Array[Snapshot],
+          sigils_tree: FileTree,
+          sorbet_intro_commit: T.nilable(String),
+          sorbet_intro_date: T.nilable(Time),
+        ).void
+      end
+      def initialize(project_name:, snapshots:, sigils_tree:, sorbet_intro_commit: nil, sorbet_intro_date: nil)
+        super(title: project_name)
+        @project_name = project_name
+        @snapshots = snapshots
+        @sigils_tree = sigils_tree
+        @sorbet_intro_commit = sorbet_intro_commit
+        @sorbet_intro_date = sorbet_intro_date
       end
 
-      sig { returns(Binding) }
-      def get_binding # rubocop:disable Naming/AccessorMethodName
-        binding
-      end
-
-      sig { returns(T::Hash[String, D3::Base]) }
-      def cards
-        {
-          "Strictness Map": D3::MapSigils.new("map_sigils", sigils_tree),
-          "Sigils Timeline": D3::TimelineSigils.new("timeline_sigils", snapshots),
-          "Calls Timeline": D3::TimelineCalls.new("timeline_calls", snapshots),
-          "Signatures Timeline": D3::TimelineSigs.new("timeline_sigs", snapshots),
-          "Sorbet Version": D3::TimelineVersions.new("timeline_versions", snapshots),
-          "Sorbet Typechecking Time": D3::TimelineRuntimes.new("timeline_runtimes", snapshots),
-        }
-      end
-
-      sig { returns(String) }
-      def erb
+      sig { override.returns(String) }
+      def header_html
+        last = T.must(snapshots.last)
         <<~ERB
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-            <meta charset="utf-8" />
-            <meta http-equiv="x-ua-compatible" content="ie=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-            <title>#{title}</title>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-
-            <style>
-              #{D3.header_style}
-            </style>
-          </head>
-          <body>
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-            <script src="https://d3js.org/d3.v4.min.js"></script>
-
-            <script>
-              #{D3.header_script}
-            </script>
-
-            <div class="px-5 py-4 container-fluid">
-              <div class="row justify-content-center">
-                <div class="col-xs-12 col-md-12 col-lg-9 col-xl-8">
-                  <h1 class="display-3">
-                    #{title}
-                    <span class="badge badge-pill badge-dark" style="font-size: 20%;">#{last.commit_sha}</span>
-                  </h1>
-                  <br>
-                  <div class="card">
-                    <h5 class="card-header">Snapshot</h5>
-                    <div class="card-body">
-                      <div class="container-fluid">
-                        <div class="row justify-content-md-center">
-                          <div class="col-12 col-sm-4 col-xl-3">
-                            #{D3::PieSigils.new('pie_sigils', 'Sigils', last).html}
-                          </div>
-                          <div class="d-none d-xl-block col-xl-1"></div>
-                          <div class="col-12 col-sm-4 col-xl-3">
-                            #{D3::PieCalls.new('pie_calls', 'Calls', last).html}
-                          </div>
-                          <div class="d-none d-xl-block col-xl-1"></div>
-                          <div class="col-12 col-sm-4 col-xl-3">
-                            #{D3::PieSigs.new('pie_sigs', 'Sigs', last).html}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <br>
-                  <% cards.each do |title, d3| %>
-                    <div class="card">
-                      <h5 class="card-header"><%= title %></h5>
-                      <div class="card-body">
-                        <%= d3.html %>
-                      </div>
-                    </div>
-                    <br>
-                  <% end %>
-                 </div>
-                </div>
-                <div class="text-center">
-                  <p>
-                    Typchecked by Sorbet since <b>#{sorbet_intro_date.strftime('%F')}</b>
-                    (commit <b>#{sorbet_intro_commit}</b>).
-                  </p>
-                  <p class="text-muted" style="margin-top: 30px;">
-                    Generated by <a href="https://github.com/Shopify/spoom">spoom</a>
-                    on #{Time.now.utc}.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </body>
-          </html>
+          <h1 class="display-3">
+            #{project_name}
+            <span class="badge badge-pill badge-dark" style="font-size: 20%;">#{last.commit_sha}</span>
+          </h1>
         ERB
+      end
+
+      sig { override.returns(T::Array[Cards::Card]) }
+      def cards
+        last = T.must(snapshots.last)
+        cards = []
+        cards << Cards::Snapshot.new(snapshot: last)
+        cards << Cards::Map.new(sigils_tree: sigils_tree)
+        cards << Cards::Timeline::Sigils.new(snapshots: snapshots)
+        cards << Cards::Timeline::Calls.new(snapshots: snapshots)
+        cards << Cards::Timeline::Sigs.new(snapshots: snapshots)
+        cards << Cards::Timeline::Versions.new(snapshots: snapshots)
+        cards << Cards::Timeline::Runtimes.new(snapshots: snapshots)
+        cards << Cards::SorbetIntro.new(sorbet_intro_commit: sorbet_intro_commit, sorbet_intro_date: sorbet_intro_date)
+        cards
       end
     end
   end
