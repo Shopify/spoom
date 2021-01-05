92fad75099a85e7811b420d1b98708bdba896a12
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2021-01-05 17:48:21 -0500

    Extract Spoon::exec
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/spoom.rb
+++ lib/spoom.rb
@@ -4,9 +4,26 @@
 require "sorbet-runtime"
 
 module Spoom
+  extend T::Sig
+
   SPOOM_PATH = (Pathname.new(__FILE__) / ".." / "..").to_s
 
   class Error < StandardError; end
+
+  sig do
+    params(
+      cmd: String,
+      arg: String,
+      path: String,
+      capture_err: T::Boolean
+    ).returns([String, T::Boolean])
+  end
+  def self.exec(cmd, *arg, path: '.', capture_err: false)
+    method = capture_err ? "popen2e" : "popen2"
+    Open3.send(method, [cmd, *arg].join(" "), chdir: path) do |_, o, t|
+      [o.read, T.cast(t.value, Process::Status).success?]
+    end
+  end
 end
 
 require "spoom/sorbet"
--- lib/spoom/sorbet.rb
+++ lib/spoom/sorbet.rb
@@ -20,22 +20,7 @@ module Spoom
 
       sig { params(arg: String, path: String, capture_err: T::Boolean).returns([String, T::Boolean]) }
       def srb(*arg, path: '.', capture_err: false)
-        opts = {}
-        opts[:chdir] = path
-        out = T.let("", T.nilable(String))
-        res = T.let(false, T::Boolean)
-        if capture_err
-          Open3.popen2e(["bundle", "exec", "srb", *arg].join(" "), opts) do |_, o, t|
-            out = o.read
-            res = T.cast(t.value, Process::Status).success?
-          end
-        else
-          Open3.popen2(["bundle", "exec", "srb", *arg].join(" "), opts) do |_, o, t|
-            out = o.read
-            res = T.cast(t.value, Process::Status).success?
-          end
-        end
-        [out || "", res]
+        T.unsafe(Spoom).exec("bundle", "exec", "srb", *arg, path: path, capture_err: capture_err)
       end
 
       sig { params(arg: String, path: String, capture_err: T::Boolean).returns([String, T::Boolean]) }
