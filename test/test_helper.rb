59b913ecfc29e36007dcdcd868ce7ebfb29ac028
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2021-10-26 17:20:59 -0400

    Remove unused test helper methods
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,21 +22,6 @@ module Spoom
       project
     end
 
-    sig { returns(String) }
-    def spoom_gemfile
-      <<~GEM
-        gem("spoom", path: "#{spoom_path}")
-      GEM
-    end
-
-    sig { returns(String) }
-    def spoom_path
-      path = File.dirname(__FILE__)   # spoom/test/spoom/
-      path = File.dirname(path)       # spoom/test/
-      path = File.dirname(path)       # spoom/
-      File.expand_path(path)
-    end
-
     # Replace all sorbet-like version "0.5.5888" in `test` by "X.X.XXXX"
     sig { params(text: String).returns(String) }
     def censor_sorbet_version(text)
