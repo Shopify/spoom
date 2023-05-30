4357d61287c612fb150e0bb680c077c692a9c4c2
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2023-05-30 12:35:59 -0400

    Introduce Context#gemfile_lock_specs
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/spoom/context/bundle_test.rb
+++ test/spoom/context/bundle_test.rb
@@ -53,6 +53,53 @@ module Spoom
         context.destroy!
       end
 
+      def test_context_read_gemfile_lock_specs_return_empty_array_if_no_gemfile_lock
+        context = Context.mktmp!
+        assert_empty(context.gemfile_lock_specs)
+        context.destroy!
+      end
+
+      def test_context_read_gemfile_lock_specs_return_specs
+        context = Context.mktmp!
+        context.write!("Gemfile.lock", <<~GEMFILE_LOCK)
+          PATH
+            remote: .
+            specs:
+              test (1.0.0)
+                sorbet (~> 0.5.5)
+                sorbet-runtime
+
+          GEM
+            remote: https://rubygems.org/
+            specs:
+              sorbet (0.5.5001)
+                sorbet-static (= 0.X.XXXX)
+              sorbet-runtime (0.5.5002)
+              sorbet-static (0.5.5003)
+
+          PLATFORMS
+            ruby
+
+          DEPENDENCIES
+            bundler (~> 1.17)
+            test!
+
+          BUNDLED WITH
+             1.17.3
+        GEMFILE_LOCK
+
+        assert_equal(
+          [
+            ["sorbet", "0.5.5001"],
+            ["sorbet-runtime", "0.5.5002"],
+            ["sorbet-static", "0.5.5003"],
+            ["test", "1.0.0"],
+          ],
+          context.gemfile_lock_specs.values.sort_by(&:name).map { |spec| [spec.name, spec.version.to_s] },
+        )
+        context.destroy!
+      end
+
       def test_context_gem_version_from_gemfile_lock_return_nil_if_no_gemfile_lock
         context = Context.mktmp!
         version = context.gem_version_from_gemfile_lock("sorbet")
