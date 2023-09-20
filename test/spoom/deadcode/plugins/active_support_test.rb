a04f375a8d00725e6a45c9fc3e9a3fbe8fcac6e5
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2023-09-20 16:18:32 -0400

    Mark methods called by ActiveSupport::TestCase.setup as alive
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

---
+++ test/spoom/deadcode/plugins/active_support_test.rb
@@ -0,0 +1,42 @@
+# typed: true
+# frozen_string_literal: true
+
+require "test_with_project"
+require "helpers/deadcode_helper"
+
+module Spoom
+  module Deadcode
+    module Plugins
+      class ActiveSupportTest < TestWithProject
+        include Test::Helpers::DeadcodeHelper
+
+        def test_ignore_minitest_setup_and_teardown_with_symbols
+          @project.write!("test/foo_test.rb", <<~RB)
+            class FooTest
+              setup(:alive1, :alive2)
+              teardown(:alive3)
+
+              def alive1; end
+              def alive2; end
+              def alive3; end
+              def dead; end
+            end
+          RB
+
+          index = index_with_plugins
+          assert_alive(index, "alive1")
+          assert_alive(index, "alive2")
+          assert_alive(index, "alive3")
+          assert_dead(index, "dead")
+        end
+
+        private
+
+        sig { returns(Index) }
+        def index_with_plugins
+          deadcode_index(plugins: [ActiveSupport.new])
+        end
+      end
+    end
+  end
+end
