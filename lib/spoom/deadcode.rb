a21105ad2f26756ea6ce6a0dd6927cbedc13135b
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-03-13 11:31:29 -0400

    Move Location as a top level concern of Spoom
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/spoom/deadcode.rb
+++ lib/spoom/deadcode.rb
@@ -5,12 +5,12 @@ require "erubi"
 require "prism"
 
 require_relative "visitor"
+require_relative "location"
 
 require_relative "deadcode/erb"
 require_relative "deadcode/index"
 require_relative "deadcode/indexer"
 
-require_relative "deadcode/location"
 require_relative "deadcode/definition"
 require_relative "deadcode/reference"
 require_relative "deadcode/send"
