7b8e04843e6d7eadd28ca6a683ea37f2f171b16d
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2025-05-14 13:23:10 -0400

    StripSorbetSigsRewriter uses `Source::Rewriter`
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/spoom/sorbet/sigs.rb
+++ lib/spoom/sorbet/sigs.rb
@@ -7,17 +7,13 @@ module Spoom
   module Sorbet
     class Sigs
       class Error < Spoom::Error; end
+
       class << self
+        # Deletes all `sig` nodes from the given Ruby code.
+        # It doesn't handle type members and class annotations.
         #: (String ruby_contents) -> String
         def strip(ruby_contents)
-          sigs = collect_sorbet_sigs(ruby_contents)
-          lines_to_strip = sigs.flat_map { |sig, _| (sig.loc&.begin_line..sig.loc&.end_line).to_a }
-
-          lines = []
-          ruby_contents.lines.each_with_index do |line, index|
-            lines << line unless lines_to_strip.include?(index + 1)
-          end
-          lines.join
+          StripSorbetSigsRewriter.new(ruby_contents, file: "test.rb").rewrite
         end
 
         #: (String ruby_contents, ?positional_names: bool) -> String
@@ -82,6 +78,75 @@ module Spoom
           visitor = SigsLocator.new
           visitor.visit(tree)
           visitor.rbs_comments.sort_by { |comment, _node| -T.must(comment.loc&.begin_line) }
+        end
+      end
+
+      # @abstract
+      class Rewriter < Spoom::Visitor
+        #: (String, file: String) -> void
+        def initialize(ruby_contents, file:)
+          super()
+
+          @file = file #: String
+
+          @ruby_contents = if ruby_contents.encoding == "UTF-8"
+            ruby_contents
+          else
+            ruby_contents.encode("UTF-8")
+          end #: String
+
+          node, comments = Spoom.parse_ruby_with_comments(ruby_contents, file: file)
+          @node = node #: Prism::Node
+          @comments_by_line = comments.to_h { |c| [c.location.start_line, c] } #: Hash[Integer, Prism::Comment]
+          @original_encoding = ruby_contents.encoding #: Encoding
+          @ruby_bytes = ruby_contents.bytes #: Array[Integer]
+          @rewriter = Spoom::Source::Rewriter.new #: Source::Rewriter
+        end
+
+        #: -> String
+        def rewrite
+          visit(@node)
+          @rewriter.rewrite!(@ruby_bytes)
+          @ruby_bytes.pack("C*").force_encoding(@original_encoding)
+        end
+
+        private
+
+        #: (Prism::CallNode node) -> bool
+        def sorbet_sig?(node)
+          return false unless node.message == "sig"
+
+          recv = node.receiver
+          return false if recv && !recv.is_a?(Prism::SelfNode) && !recv.slice.match?(/(::)?T::Sig::WithoutRuntime/)
+
+          true
+        end
+
+        #: (Integer) -> Integer
+        def adjust_to_line_start(offset)
+          offset -= 1 while offset > 0 && @ruby_bytes[offset - 1] != "\n".ord
+          offset
+        end
+
+        #: (Integer) -> Integer
+        def adjust_to_line_end(offset)
+          offset += 1 while offset < @ruby_bytes.size && @ruby_bytes[offset] != "\n".ord
+          offset
+        end
+      end
+
+      # Deletes all `sig` nodes from the given Ruby code.
+      # It doesn't handle type members and class annotations.
+      class StripSorbetSigsRewriter < Rewriter
+        # @override
+        #: (Prism::CallNode node) -> void
+        def visit_call_node(node)
+          return unless sorbet_sig?(node)
+
+          @rewriter << Source::Delete.new(
+            adjust_to_line_start(node.location.start_offset),
+            adjust_to_line_end(node.location.end_offset),
+          )
         end
       end
 
