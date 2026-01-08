dfcc5b1fb09e9b8e3d0215cd74952bc17b4a030f
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2026-01-08 11:16:02 -0500

    Rewrite `Deadcode::Send` as a bare Ruby class
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/spoom/deadcode/send.rb
+++ lib/spoom/deadcode/send.rb
@@ -4,13 +4,41 @@
 module Spoom
   module Deadcode
     # An abstraction to simplify handling of Prism::CallNode nodes.
-    class Send < T::Struct
-      const :node, Prism::CallNode
-      const :name, String
-      const :recv, T.nilable(Prism::Node), default: nil
-      const :args, T::Array[Prism::Node], default: []
-      const :block, T.nilable(Prism::Node), default: nil
-      const :location, Location
+    class Send
+      #: Prism::CallNode
+      attr_reader :node
+
+      #: String
+      attr_reader :name
+
+      #: Prism::Node?
+      attr_reader :recv
+
+      #: Array[Prism::Node]
+      attr_reader :args
+
+      #: Prism::Node?
+      attr_reader :block
+
+      #: Location
+      attr_reader :location
+
+      #: (
+      #|   node: Prism::CallNode,
+      #|   name: String,
+      #|   location: Location,
+      #|   ?recv: Prism::Node?,
+      #|   ?args: Array[Prism::Node],
+      #|   ?block: Prism::Node?
+      #| ) -> void
+      def initialize(node:, name:, location:, recv: nil, args: [], block: nil)
+        @node = node
+        @name = name
+        @recv = recv
+        @args = args
+        @block = block
+        @location = location
+      end
 
       #: [T] (Class[T] arg_type) { (T arg) -> void } -> void
       def each_arg(arg_type, &block)
